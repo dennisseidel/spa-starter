94d64cbfa949108977a159ccf4ad409fd0c317ea
Dennis Seidel <den.seidel@gmail.com>
2018-03-03 21:23:52 +0100

    feat: add new component stub

--- src/components/NavBar/NavBar-story.js
+++ src/components/NavBar/NavBar-story.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import NavBar from './NavBar';
+import NavBarNew from './NavBarNew';
 import logo from '../../containers/App/ecosphere-logo.svg';
 
 storiesOf('NavBar', module)
@@ -53,3 +54,54 @@ storiesOf('NavBar', module)
     ]
     return <NavBar logo={logo} width='150' height='25' elements={elements} rightElements={rightElements} auth={auth} />
   });  
+
+
+  storiesOf('NavBarNew', module)
+  .add('authenticated', () => {
+    let elements = [
+      {
+        name: "Home",
+        url: "/home"
+      },
+      {
+        name: "Services",
+        url: "/service"
+      }
+    ];
+    let rightElements = [
+      {
+        name: "Manage",
+        url: "/manage"
+      }
+    ]
+    let auth = {
+      isAuthenticated: true
+    }
+    return <NavBarNew logo={logo} width='150' height='25' elements={elements} rightElements={rightElements} auth={auth} />
+  })
+  .add('unauthenticated', () => {
+    let elements = [
+      {
+        name: "Home",
+        url: "/home"
+      },
+      {
+        name: "Services",
+        url: "/service"
+      },
+      {
+        name: "Bobshow",
+        url: "/bobshow"
+      }
+    ];
+    let auth = {
+      isAuthenticated: false
+    }
+    let rightElements = [
+      {
+        name: "Manage",
+        url: "/manage"
+      }
+    ]
+    return <NavBarNew logo={logo} width='150' height='25' elements={elements} rightElements={rightElements} auth={auth} />
+  }); 
---
+++ src/components/NavBar/NavBarNew.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+const navbar = (props) => {
+  return (
+    <div>test</div>
+  )
+
+}
+
+export default navbar;
