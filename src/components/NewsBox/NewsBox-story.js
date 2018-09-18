76fd5a209a580aa20ebdb17077fbb48841f6dfdf
Dennis Seidel <den.seidel@gmail.com>
2018-09-18 09:00:27 +0200

    fix: missing file reference in storyboard

--- src/components/NewsBox/NewsBox-story.js
+++ src/components/NewsBox/NewsBox-story.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import NewsBox from './NewsBox';
-import picture from '../../containers/Home/title.png'
+//import picture from '../../containers/Home/title.png'
 
 storiesOf('NewsBox', module)
   .add('basic', () => {
@@ -10,7 +10,7 @@ storiesOf('NewsBox', module)
       title: "Introducing Poly API",
       summary: "The Poly API enables your application to find and import 3D assets easily and directly from the Poly library. The REST API is available for any platform. If you're building on top of Unity or Unreal Engine make sure to check out the Poly Toolkit, which will make your workflow even more efficient.",
       button: "Learn more",
-      picture: picture
+      picture: null
     }
     return <NewsBox picture={entry.picture} title={entry.title} summary={entry.summary} button={entry.button} />
   });
